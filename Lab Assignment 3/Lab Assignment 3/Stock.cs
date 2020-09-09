6b56425ca2f431abe32e7a373296f09eb73a5172
jda_1 <jose.aceves95@gmail.com>
2020-09-08 22:19:00 -0700

    added comments

--- Lab Assignment 3/Lab Assignment 3/Stock.cs
+++ Lab Assignment 3/Lab Assignment 3/Stock.cs
@@ -6,19 +6,25 @@ namespace Stock
 {
     public class Stock
     {
+        // Declare EventHandler of type Stock Notification
         public event EventHandler<StockNotification> StockEvent;
+
+        // Declare ReaderWriterLockSlim to lock writes to a shared resource from multiple threads.
+        // Writes to a text file will include date & time, stock name, stock initial value, and stock price.
         public static ReaderWriterLockSlim myLock = new ReaderWriterLockSlim();
         public static readonly string docPath = @"C:\Users\Tanner\Documents\Github\Lab-Assignment-3\Lab Assignment 3\Lab Assignment 3\Lab3_output.txt";
         public static readonly string title = "Date and Time".PadRight(30) + "Stock".PadRight(15) +
                 "Initial Value".PadRight(15) + "Price";
         private readonly Thread _thread;
 
+        // Member variables. 
         public string StockName { get; set; }
         public int InitialValue { get; set; }
         public int CurrentValue { get; set; }
         public int MaxChange { get; set; }
         public int Threshold { get; set; }
         public int NumChanges { get; set; }
+
         /// <summary>
         /// Stock class that contains all the information and changes of the stock
         /// </summary>
@@ -62,7 +68,11 @@ namespace Stock
                 StockEvent?.Invoke(this, null);
             }
         }
-
+        /// <summary>
+        ///  The eventhandler that saves a stock's information to an output file.
+        /// </summary>
+        /// <param name="sender">The sender that indicated a change</param>
+        /// <param name="e">Event arguments</param>
         protected void FileEventHandler(object sender, EventArgs e)
         {
             myLock.EnterWriteLock();
